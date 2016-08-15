b80181ea5dddbfc4fca1e38769e374791be148e6
vyvojar <vyvojar@mail.ru>
2016-08-15 16:16:18 +0200

    support for kernel value reporting

--- pd.c
+++ pd.c
@@ -4,6 +4,9 @@
 #include <winternl.h>
 #include "wind.h"
 
+#define QUERY_KERNEL 1
+
+NTSTATUS NTAPI NtQueryLicenseValue(PUNICODE_STRING,DWORD*,PVOID,DWORD,DWORD*);
 int main()
 {
 	HKEY hk;
@@ -21,18 +24,31 @@ int main()
 	for (int i = 0; i < n; i++) {
 		wind_pol_ent *e = ar[i];
 		BYTE *d = e->name + e->name_sz;
+		DWORD t = e->type;
+		DWORD l = e->data_sz;
+#if QUERY_KERNEL
+		WCHAR buf[512];
+		UNICODE_STRING us;
+		memcpy(buf, e->name, e->name_sz);
+		buf[e->name_sz/2] = 0;
+		RtlInitUnicodeString(&us, buf);
+		l = sizeof(buf);
+		if (!NT_SUCCESS(NtQueryLicenseValue(&us, &t, buf, l, &l)))
+			continue;
+		d = (void*)buf;
+#endif
 		printf("\"%.*S\"=", e->name_sz/2, (WCHAR*)e->name);
-		switch (e->type) {
+		switch (t) {
 			case REG_DWORD:
 				printf("dword:%08x\n", *(DWORD*)d);
 				break;
 			case REG_SZ:
-				printf("\"%.*S\"\n", e->data_sz/2, (WCHAR*)d);
+				printf("\"%.*S\"\n", l/2, (WCHAR*)d);
 				break;
 			case REG_BINARY:
 				printf("hex:");
-				for (int j = 0; j < e->data_sz; j++)
-					printf("%02hhx%c",d[j],j!=e->data_sz-1?',':'\n');
+				for (int j = 0; j < l; j++)
+					printf("%02hhx%c",d[j],j!=l-1?',':'\n');
 				break;
 			default:
 				printf(";Unknown policy type %d\n",e->type);
